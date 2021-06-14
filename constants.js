42d343db5021155fe5c63515adb1c7781163cdbb
Anupam Dahal <anupamdahal1@gmail.com>
2021-06-14 02:11:13 -0500

    Delete constants.js

--- constants.js
+++
@@ -1,9 +0,0 @@
-const constants = {
-  bufferSize: (2*1024*1024*1024),
-  noOfActiveFetchLimit: 6,
-  fallbackNoOfActiveFetchLimit: 2,
-  downloadInterval: 1000,
-  fallbackDownloadInterval: 10000
-}
-
-export default constants
